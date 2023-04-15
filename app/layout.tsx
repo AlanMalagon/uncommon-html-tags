fe71e7c1fbbe4cedfba6ffc9d70f73c18cc52e18
alanmalagon <alan.s.malagon@hotmail.com>
2023-04-15 14:35:59 -0600

    feat: added basic home page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata = {
@@ -6,6 +7,17 @@ export const metadata = {
     "Website to highlight the power of vanilla HTML for modern web development.",
 };
 
+const links = [
+  {
+    href: "/examples/datalist",
+    label: "Examples",
+  },
+  {
+    href: "/",
+    label: "About",
+  },
+];
+
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +25,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="h-screen bg-gradient-to-r from-slate-950  to-pink-950">
+        <header className="sticky w-full h-14 top-0 flex place-items-center px-4">
+          <Link
+            href="/"
+            className="pr-8 text-lg project-name text-shadow-pink-500/75 border-r border-slate-800"
+          >
+            {metadata.title}
+          </Link>
+          <div className="flex flex-1 gap-8 pl-8">
+            {links.map(({ href, label }) => (
+              <Link
+                key={href}
+                href={href}
+                className="text-white shadow-sm hover:text-pink-300 transition-colors"
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
