53da0bb117575369d047b561ecaf6abb68fddac5
alanmalagon <alan.s.malagon@hotmail.com>
2023-04-20 18:16:06 -0600

    feat: mobile menu now closes after clicking a link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { metadata } from "@/app/layout";
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 
 const links = [
@@ -12,49 +13,54 @@ const links = [
   },
 ];
 
+const PageName = "Uncommon HTML Tags";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="sticky w-full h-14 top-0 flex place-items-center px-4 3xl:h-20">
+      {/* Desktop Home Link */}
       <Link
         href="/"
         className="hidden text-sm project-name text-shadow-pink-500/75 md:pr-8  md:text-lg md:border-r md:border-slate-800 
         3xl:text-3xl 3xl:ml-4 md:block"
       >
-        <p className="">{metadata.title}</p>
+        {PageName}
       </Link>
 
-      {/* Menu icon */}
-      {/* 
-        Tailwind CSS allows using the 'peer' class to enable a kind of listener 
-        on a sibling element (at the same nested level).
-        
-        After that, the 'peer-checked' selector can be applied applied in another element
-        so it can listen to its 'peer', here the 'menu' class (defined in global.css) includes it,
-        therefore, when the checkbox is checked, it will show the menu by applying opacity-100.
-
-        Important to use the htmlFor attribute on the label so it checks the hidden input
-        whenever it is clicked.
-      */}
-      <input type="checkbox" className="peer hidden" id="menu-toggle" />
+      {/* Menu Icon only on mobile*/}
       <label
-        htmlFor="menu-toggle"
-        className="block text-4xl project-name text-shadow-pink-500/75 md:hidden"
+        className="block text-4xl project-name hover:text-shadow-pink-500/75 md:hidden cursor-pointer"
+        onClick={() => setMenuOpen((current) => !current)}
       >
         &#9776;
       </label>
 
-      <div className="menu">
+      {/* Menu Container for both Desktop and Mobile */}
+      <div className={`menu ${menuOpen ? " opacity-100" : ""}`}>
+        {/* Mobile Home Link */}
         <Link
           href="/"
-          className="project-name text-shadow-pink-500/75 md:hidden"
+          className={`project-name text-shadow-pink-500/75 md:hidden ${
+            menuOpen ? "visible" : "invisible"
+          }`}
+          onClick={handleMenuClose}
         >
-          <p className="">{metadata.title}</p>
+          {PageName}
         </Link>
         {links.map(({ href, label }) => (
           <Link
             key={href}
             href={href}
-            className="text-white hover:text-pink-300 transition-colors"
+            className={`text-white hover:text-pink-300 transition-colors md:visible ${
+              menuOpen ? "visible" : "invisible"
+            }`}
+            onClick={handleMenuClose}
           >
             {label}
           </Link>
