8dfda14a98db48f2070ba393624261863ab141ef
alanmalagon <alan.s.malagon@hotmail.com>
2023-05-15 17:00:29 -0700

    chore: CodeViewer accept a component as child to show its code

--- app/examples/datalist/page.tsx
+++ app/examples/datalist/page.tsx
@@ -1,62 +1,52 @@
 import CodeViewer from "@/components/CodeViewer/CodeViewer";
 import React from "react";
 
-// TODO: Think a better way to save this alongside its live example
-const text = `<label htmlFor="favorite-anime-input" className="text-pink-300">
-  Favorite anime:
-</label>
-<input
-  list="favorite-anime-list"
-  id="favorite-anime-input"
-  name="favorite-anime-input"
-  className="h-11 px-1 rounded md:w-64 md:h-auto"
-/>
+const Demo = () => (
+  <>
+    <label htmlFor="favorite-anime-input" className="text-pink-300">
+      Favorite anime:
+    </label>
+    <input
+      list="favorite-anime-list"
+      id="favorite-anime-input"
+      name="favorite-anime-input"
+      className="h-11 px-1 rounded md:w-64 md:h-auto"
+    />
 
-<datalist id="favorite-anime-list">
-  <option value="Haikyuu" />
-  <option value="My Hero Academia" />
-  <option value="Full Metal Alchemist" />
-  <option value="Attack On Titan" />
-  <option value="Sesame Street" />
-</datalist>
-
-// Justification for Sesame Street being an anime: https://twitter.com/RiseFallNickBck/status/1482563258510946308
-`;
+    <datalist id="favorite-anime-list">
+      <option value="Haikyuu" />
+      <option value="My Hero Academia" />
+      <option value="Full Metal Alchemist" />
+      <option value="Attack On Titan" />
+      <option value="Sesame Street" />
+    </datalist>
+  </>
+);
 
 const DataList = () => {
   return (
     <main className="p-2">
-      <section>
+      <section className="text-white [&>p]:px-4 [&>p]:py-1">
         <h1 className="project-name text-2xl">{`<datalist>`}</h1>
-        <p className="text-white p-4">
+        <p className="">
           The datalist element is really useful, you can create a
           dropdown-like interaction without having to use JS or any
-          component library. Down below you can find an example of it in
-          action:
+          component library.
+        </p>
+        <p>
+          Downside of this tag is that is not completely compatible with
+          all browsers, it will work fine with most of the common ones like
+          Chrome, Firefox, Safari, and Edge.
         </p>
       </section>
       <section>
         <h1 className="project-name text-2xl w-full">Example</h1>
         <div className="flex flex-col gap-2 p-4 md:flex-row">
-          <label htmlFor="favorite-anime-input" className="text-pink-300">
-            Favorite anime:
-          </label>
-          <input
-            list="favorite-anime-list"
-            id="favorite-anime-input"
-            name="favorite-anime-input"
-            className="h-11 px-1 rounded md:w-64 md:h-auto"
-          />
-
-          <datalist id="favorite-anime-list">
-            <option value="Haikyuu" />
-            <option value="My Hero Academia" />
-            <option value="Full Metal Alchemist" />
-            <option value="Attack On Titan" />
-            <option value="Sesame Street" />
-          </datalist>
+          <Demo />
         </div>
-        <CodeViewer ariaLabel="datalist-example">{text}</CodeViewer>
+        <CodeViewer ariaLabel="datalist-example" className="break-words">
+          <Demo />
+        </CodeViewer>
       </section>
     </main>
   );
--- components/CodeViewer/CodeViewer.tsx
+++ components/CodeViewer/CodeViewer.tsx
@@ -1,15 +1,23 @@
 "use client";
-import { FC, ReactNode, useEffect } from "react";
+import { FC, ReactElement, ReactNode, useEffect } from "react";
+
+// Highlight.js to make the code colorful based on synstax.
 import "highlight.js/styles/base16/ros-pine-moon.css";
 import hljs from "highlight.js/lib/core";
 import xml from "highlight.js/lib/languages/xml";
+
+// Code formatting.
+import { renderToStaticMarkup } from "react-dom/server";
+import htmlParser from "prettier/parser-html";
+import prettier from "prettier";
+
 hljs.registerLanguage("html", xml);
 
-// Make it have the live example embedded with the code in tabs
-const CodeViewer: FC<{ children: ReactNode; ariaLabel: string }> = ({
-  children,
-  ariaLabel,
-}) => {
+const CodeViewer: FC<{
+  children: ReactNode;
+  ariaLabel: string;
+  className: string;
+}> = ({ children, ariaLabel, className }) => {
   useEffect(() => {
     hljs.initHighlighting();
   }, []);
@@ -19,7 +27,15 @@ const CodeViewer: FC<{ children: ReactNode; ariaLabel: string }> = ({
       aria-label={ariaLabel}
       className="p-4 overflow-scroll text-white text-xs"
     >
-      <code className="html rounded">{children}</code>
+      <code className={`html rounded ${className}`}>
+        {prettier.format(renderToStaticMarkup(children as ReactElement), {
+          parser: "html",
+          plugins: [htmlParser],
+          htmlWhitespaceSensitivity: "ignore",
+          printWidth: 60,
+        })}
+        ;
+      </code>
     </pre>
   );
 };
