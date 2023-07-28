923acbb005bf0afca757903b33333f94be6d23bd
Alan Malagón <alan.s.malagon@hotmail.com>
2023-07-28 02:46:36 -0600

    feat: updated datalist information to include a list

--- app/examples/datalist/page.tsx
+++ app/examples/datalist/page.tsx
@@ -6,18 +6,22 @@ import Title from "@/components/Title";
 const DataList = () => {
   return (
     <main className="p-2">
-      <section className="text-white [&>p]:px-4 [&>p]:py-1">
+      <section className="text-white [&>ul]:px-12 [&>p]:px-4 [&>p]:py-1">
         <Title>Datalist</Title>
         <p className="">
           The datalist element is quite useful, you can create a
           dropdown-like interaction without having to use JS or any
           component library.
         </p>
-        <p>
-          Downside of this tag is that is not completely compatible with
-          all browsers, it will work fine with most of the common ones like
-          Chrome, Firefox, Safari, and Edge.
-        </p>
+        <p>Downsides of this tag:</p>
+        <ul className="list-disc">
+          <li>
+            Not completely compatible with all browsers, it will work fine
+            with most of the common ones like Chrome, Firefox, Safari, and
+            Edge.
+          </li>
+          <li>It is not CSS-customizable.</li>
+        </ul>
       </section>
       <section>
         <Title variant="h2">Example</Title>
